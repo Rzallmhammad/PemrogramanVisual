001699e28fea67ee91644426fe88a42065533b3b
Rzallmhammad <139690181+Rzallmhammad@users.noreply.github.com>
2024-02-17 15:33:55 +0800

    Add files via upload

---
+++ AbsensiMahasiswa/AbsensiMahasiswa/Form1.Designer.vb
@@ -0,0 +1,233 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.txtProdi = New System.Windows.Forms.TextBox()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtNIM = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.Controls.Add(Me.Panel4)
+        Me.Panel1.Controls.Add(Me.Panel3)
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Location = New System.Drawing.Point(11, 16)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(867, 528)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel4.Controls.Add(Me.btnExit)
+        Me.Panel4.Controls.Add(Me.btnDelete)
+        Me.Panel4.Controls.Add(Me.btnSave)
+        Me.Panel4.Location = New System.Drawing.Point(673, 77)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(180, 438)
+        Me.Panel4.TabIndex = 2
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(28, 310)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(123, 90)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(28, 169)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(123, 90)
+        Me.btnDelete.TabIndex = 1
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(28, 40)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(123, 90)
+        Me.btnSave.TabIndex = 0
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel3.Controls.Add(Me.DataGridView1)
+        Me.Panel3.Controls.Add(Me.txtProdi)
+        Me.Panel3.Controls.Add(Me.txtNama)
+        Me.Panel3.Controls.Add(Me.txtNIM)
+        Me.Panel3.Controls.Add(Me.Label4)
+        Me.Panel3.Controls.Add(Me.Label3)
+        Me.Panel3.Controls.Add(Me.Label2)
+        Me.Panel3.Location = New System.Drawing.Point(12, 77)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(641, 438)
+        Me.Panel3.TabIndex = 1
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.Control
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(34, 246)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(571, 168)
+        Me.DataGridView1.TabIndex = 7
+        '
+        'txtProdi
+        '
+        Me.txtProdi.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtProdi.Location = New System.Drawing.Point(168, 169)
+        Me.txtProdi.Name = "txtProdi"
+        Me.txtProdi.Size = New System.Drawing.Size(437, 32)
+        Me.txtProdi.TabIndex = 6
+        '
+        'txtNama
+        '
+        Me.txtNama.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNama.Location = New System.Drawing.Point(168, 99)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(437, 32)
+        Me.txtNama.TabIndex = 5
+        '
+        'txtNIM
+        '
+        Me.txtNIM.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNIM.Location = New System.Drawing.Point(168, 30)
+        Me.txtNIM.Name = "txtNIM"
+        Me.txtNIM.Size = New System.Drawing.Size(437, 32)
+        Me.txtNIM.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(36, 173)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Prodi"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(36, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nama"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(36, 36)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "NIM"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Location = New System.Drawing.Point(12, 12)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(841, 47)
+        Me.Panel2.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(316, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(248, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Absensi Mahasiswa"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(890, 556)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtProdi As TextBox
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents txtNIM As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnSave As Button
+End Class
---
+++ AbsensiMahasiswa/AbsensiMahasiswa/Form1.vb
@@ -0,0 +1,118 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+    Dim conn As New OleDbConnection
+    Dim cmd As OleDbCommand
+    Dim dt As New DataTable
+    Dim da As New OleDbDataAdapter()
+
+    ' Procedure untuk menampilkan data pada DataGridView
+    Private Sub viewer()
+        conn.Open()
+        cmd = conn.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        da = New OleDbDataAdapter("select * from vbsavedata", conn)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+        conn.Close()
+    End Sub
+
+    ' Procedure yang dijalankan saat form dimuat
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Mengatur koneksi ke basis data MS Access
+        conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\belajar\AbsensiMahasiswa\AbsensiMahasiswa\bin\Debug\vbsavedata.accdb"
+        ' Menampilkan data pada DataGridView
+        viewer()
+    End Sub
+
+    ' Procedure yang dijalankan saat tombol "Save" ditekan
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Try
+            conn.Open()
+            cmd = conn.CreateCommand()
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "INSERT INTO vbsavedata (NIM, Nama, Prodi) VALUES ('" + txtNIM.Text + "','" + txtNama.Text + "', '" + txtProdi.Text + "')"
+            cmd.ExecuteNonQuery()
+            conn.Close()
+            MessageBox.Show("Record Saved MS Access", "Vb Save Database", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            ' Membersihkan dan mengisi kembali DataTable
+            dt.Clear()
+            da.Fill(dt)
+            ' Merefresh DataGridView
+            DataGridView1.Refresh()
+
+            ' Mengosongkan TextBox
+            txtNIM.Text = ""
+            txtNama.Text = ""
+            txtProdi.Text = ""
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Vb Save Database", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            conn.Close()
+        End Try
+    End Sub
+
+    ' Procedure yang dijalankan saat sel pada DataGridView diklik
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        Try
+            txtNIM.Text = DataGridView1.SelectedRows(0).Cells(0).Value.ToString()
+            txtNama.Text = DataGridView1.SelectedRows(0).Cells(1).Value.ToString()
+            txtProdi.Text = DataGridView1.SelectedRows(0).Cells(2).Value.ToString()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Vb Save Database", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            conn.Close()
+        End Try
+    End Sub
+
+    ' Procedure yang dijalankan saat tombol "Delete" ditekan
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Try
+            ' Validasi NIM sebagai angka sebelum menghapus
+            If Not Integer.TryParse(txtNIM.Text, Nothing) Then
+                MessageBox.Show("NIM harus berupa nilai numerik.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
+            conn.Open()
+            cmd = conn.CreateCommand()
+            cmd.CommandType = CommandType.Text
+
+            ' Menggunakan parameterized query untuk mencegah SQL Injection
+            cmd.CommandText = "DELETE FROM vbsavedata WHERE NIM = @NIM"
+            cmd.Parameters.AddWithValue("@NIM", CInt(txtNIM.Text)) ' Konversi ke tipe data yang sesuai
+
+            cmd.ExecuteNonQuery()
+            conn.Close()
+
+            MessageBox.Show("Record Successfully Deleted", "Vb Save Database", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            ' Membersihkan dan mengisi kembali DataTable
+            dt.Clear()
+            da.Fill(dt)
+            ' Merefresh DataGridView
+            DataGridView1.Refresh()
+
+            ' Mengosongkan TextBox
+            txtNIM.Text = ""
+            txtNama.Text = ""
+            txtProdi.Text = ""
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Vb Save Database", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            conn.Close()
+        End Try
+    End Sub
+
+    ' Procedure yang dijalankan saat tombol "Exit" ditekan
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim iExit As DialogResult
+
+        iExit = MessageBox.Show("confirm if you want to exit", "Vb Save Database", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+
+        If iExit = DialogResult.Yes Then
+            Application.Exit()
+        End If
+    End Sub
+End Class
